8eea03653b0add4a1287f486758d2616e975be20
UselessMurder <akmarkov45@gmail.com>
2021-09-12 01:47:24 +0300

    t

--- start.cmd
+++ start.cmd
@@ -2,6 +2,6 @@ powershell -Command "& {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned}"
 powershell -NoProfile -ExecutionPolicy ByPass -File explorer.ps1
 powershell -NoProfile -ExecutionPolicy ByPass -File setup_choco.ps1
 
-setx /M path %path%;%programdata%\chocolatey\tools\
+REM setx /M path %path%;%programdata%\chocolatey\tools\
 refreshenv
 powershell -NoProfile -ExecutionPolicy ByPass -File setup_base.ps1
