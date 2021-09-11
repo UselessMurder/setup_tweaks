7648309cb8a8be261fcb8c307f0c14d368832acd
UselessMurder <akmarkov45@gmail.com>
2021-09-12 02:19:31 +0300

    fixes

--- cure.cmd
+++ cure.cmd
@@ -1,6 +1,7 @@
 cd .\plugins
-7z x cure.zip
+powershell -Command $pword = read-host "Enter password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p cure_password=<.tmp.txt & del .tmp.txt
+7z x -p%cure_password% cure.7zip
 cd .\cure
-.\run.cmd
+call .\run.cmd
 cd ..
 rd /s /q .\cure
